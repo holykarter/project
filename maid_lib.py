b921957833d4270ce0cee2f1983d01fe11ddc026
holykarter <el.holy.karter@gmail.com>
2025-09-02 02:23:02 -0400

    Create maid_lib.py
    crafted the idea for the code

---
+++ maid_lib.py
@@ -0,0 +1,12 @@
+''' 
+phrase, three empty space , can be fill with an f-strings 
+get the input of the users
+phrase = f'somthing {} and {}'
+take a tuple put the variable to compare with the user one , 
+
+phrase = f'thise man {var1}, something to {var2}
+print('so the game can begin')
+
+var1 = input{prosser par par les valeur ci dessus )
+
+'''
